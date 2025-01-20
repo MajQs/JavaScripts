e5dcdaaa5871e25d5348b4c1222d375cf6aa33c2
Mateusz Majkowski <majkowski07@gmail.com>
2025-01-20 17:57:15 +0100

    init

--- plemiona/plemiona.js
+++ plemiona/plemiona.js
@@ -1,21 +1,22 @@
+//  Loop: farm -> scavenger -> wrecker
 // === PARAMS ===
 var conf = {
   farm: {
-    enabled: 1
+    enabled: 1,
     speedInMilliseconds: 1000,      // speedInMilliseconds +-250ms
     repeatWhenNoMoreVillagesLeft: 0 // 0 -> idzie do zbieraka, 1 -> wraca na pierwszą strone
   },
   wrecker: {
     enabled: 1
   },
-  scavenger = {
-    enabled: 1
+  scavenger: {
+    enabled: 1,
     durationInMin: 30               // minuty spędzone na zbieraku
   }
 }
 
 // ==============
-$.getScript('https://cdn.jsdelivr.net/gh/MajQs/JavaScripts@3d74eae/plemiona/util.js');
-$.getScript('https://cdn.jsdelivr.net/gh/MajQs/JavaScripts@3d74eae/plemiona/rozkazy.js');
-$.getScript('https://cdn.jsdelivr.net/gh/MajQs/JavaScripts@3d74eae/plemiona/asystent_farmera.js');
-$.getScript('https://cdn.jsdelivr.net/gh/MajQs/JavaScripts@3d74eae/plemiona/zbierak.js');
+$.getScript('https://cdn.jsdelivr.net/gh/MajQs/JavaScripts@790c1cd/plemiona/util.js');
+$.getScript('https://cdn.jsdelivr.net/gh/MajQs/JavaScripts@790c1cd/plemiona/rozkazy.js');
+$.getScript('https://cdn.jsdelivr.net/gh/MajQs/JavaScripts@790c1cd/plemiona/asystent_farmera.js');
+$.getScript('https://cdn.jsdelivr.net/gh/MajQs/JavaScripts@790c1cd/plemiona/zbierak.js');
