7ea2b5b334e605c2ec3dc4c39893dea6e95e8a39
Mateusz Majkowski <majkowski07@gmail.com>
2025-01-20 12:57:54 +0100

    init

--- plemiona/asystent_farmera.js
+++ plemiona/asystent_farmera.js
@@ -1,5 +1,54 @@
+function processWrecker(delay) {
+   console.log("Processing Wrecker..." );
+   var coordinatesForWrecker = []
 
- function processAF(delay) {
+   let rows = $(`#plunder_list tr`).slice(2);
+
+   function processRowWithDelay(index) {
+     // go to next page
+     if (index >= rows.length) {
+       let firstColumnElements = $(`#plunder_list_nav tr`).eq(0).find('td').eq(0).children();
+       let strongElement = firstColumnElements.filter('strong');
+
+       if (strongElement.length > 0) {
+         let nextAnchor = strongElement.next('a');
+         if (nextAnchor.length > 0) {
+           nextAnchor[0].click(); // next page
+         } else {
+           console.log("CoordinatesForWrecker: " + cordsForWrecker);
+         }
+       }
+       return;
+     }
+
+     if ($(rows[index]).find('td').eq(1).find('img').first().attr('src').indexOf('red') > -1 ) { // defeated
+       if ($(rows[index]).find('td').eq(3).find('img').length == 0){ // no attack is coming
+         var coordinates = $(rows[index]).find('td').eq(3).find('a').first().val()
+         cordsForWrecker.push(coordinates.substr(coordinates.indexOf("("), coordinates.indexOf(")")))
+       }
+     }
+
+     setTimeout(function() {
+       // some error?
+       if ($('div.autoHideBox.error').length > 0) {
+//         goToScavenge()
+         return;
+       }
+
+       // next row
+       processRowWithDelay(index + 1);
+     }, delay);
+   }
+
+   // start AF
+   setTimeout(function() {
+     processRowWithDelay(0);
+   }, 3000)
+
+ }
+
+ function processFarm(delay) {
+   console.log("Processing Farm..." );
    let rows = $(`#plunder_list tr`).slice(2);
 
    function processRowWithDelay(index) {
@@ -41,14 +90,13 @@
 
        // next row
        processRowWithDelay(index + 1);
-     }, getRandomDelay(500, 1200));
+     }, delay);
    }
 
    // start AF
    setTimeout(function() {
      processRowWithDelay(0);
    }, 3000)
-
  }
 
  function isAF() {
@@ -60,5 +108,6 @@
 
  if (isAF()) {
    console.log("Processing AF..." );
-   processAF();
+   processWrecker(100);
+//   processFarm(getRandomDelay(500, 1200));
  }
