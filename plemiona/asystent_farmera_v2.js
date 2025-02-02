bf1b0f1cc0089b62b74262e582340336dc301d96
Mateusz Majkowski <majkowski07@gmail.com>
2025-02-02 19:26:07 +0100

    v2 wrecker test

--- plemiona/asystent_farmera_v2.js
+++ plemiona/asystent_farmera_v2.js
@@ -63,7 +63,8 @@ function processCollectAFStatistics() {
             for (let pvi = playerVillages.length-1; pvi >= 0; pvi--) {
                 var distance = Math.sqrt(Math.pow(coords[0]-playerVillages[pvi][1].X,2)+Math.pow(coords[1]-playerVillages[pvi][1].Y,2))
                 if(distance <= conf.farm.wrecker.maxDistance    // is in rage of max distance
-                    && playerVillages[pvi][1].isWrecker)        // is wrecker
+                    && playerVillages[pvi][1].isWrecker
+                    && !isVillageWithFrozenOff())        // is wrecker
                 {
                     var current = coordinatesForWrecker.get(coordinates)
                     if(current == null){
